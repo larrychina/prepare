1488512cd91e3116d27c99c0cfbd307ac8708419
larrychina <935031752@qq.com>
2020-07-20 20:44:11 +0800

    linklist reverse

--- src/main/java/org/larry/normal/linklist/LinkList.java
+++ src/main/java/org/larry/normal/linklist/LinkList.java
@@ -5,9 +5,35 @@ package org.larry.normal.linklist;
  */
 public class LinkList<T> {
 
+    Node linkList  ;
+
+
+    /**
+     * 链表反转
+     * @param linkList
+     * @return
+     */
+    public static Node reverseNodeList(Node linkList){
+        if(linkList == null){
+            return null ;
+        }
+        Node node = linkList ;
+        Node reverse = null ;
+        Node tmpNode = null ;
+        while (node != null){
+            tmpNode = node ;
+            node = node.next;
+            tmpNode.next = reverse;
+            reverse = tmpNode ;
+        }
+        return reverse ;
+    }
+
+
+
     public static class Node<T>{
         private T data ;
-        private FindCycleLinkList.Node next ;
+        private Node next ;
 
         public T getData() {
             return data;
@@ -17,13 +43,14 @@ public class LinkList<T> {
             this.data = data;
         }
 
-        public FindCycleLinkList.Node getNext() {
+        public Node getNext() {
             return next;
         }
 
-        public void setNext(FindCycleLinkList.Node next) {
+        public void setNext(Node next) {
             this.next = next;
         }
+
 
         public Node() {
         }
@@ -32,5 +59,26 @@ public class LinkList<T> {
             this.data = data;
         }
     }
+
+    public static void main(String[] args) {
+        Node node = null ;
+        Node tmp = node ;
+        for (int i = 0; i < 10; i++) {
+            if(tmp == null){
+                node = new Node(i);
+                tmp = node ;
+            } else{
+                tmp.next = new Node(i) ;
+                tmp = tmp.next ;
+            }
+        }
+        Node revese = reverseNodeList(node) ;
+        while (revese != null){
+            System.out.print(revese.getData()+",");
+            revese= revese.next ;
+
+        }
+    }
+
 
 }
